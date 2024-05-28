444f4e8b67a937e14405a0ff8e1bb6ee5c959163
Max Viehl <max.viehl@posteo.de>
2024-05-28 10:40:08 +0200

    Rename ROS workspace, fix ros2 launch command
    Do not call it rslidar because one may install other packages later

--- build/files/entrypoint.sh
+++ build/files/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # setup ros environment, this is copied from the entrypoint script of the normal ROS Dockerfile
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
-# also, source the robosense sdk
-source "/robosense/install/setup.bash"
+# also, source our workspace
+source "/ROS_WORKSPACE/install/setup.bash"
 
 exec "$@"
