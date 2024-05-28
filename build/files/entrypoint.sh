d1cd52d63299778c435ca454e08041809d6931b0
Max Viehl <max.viehl@posteo.de>
2024-05-28 09:52:51 +0200

    Switch to Ros2 humble based RSLidar SDK build

--- build/files/entrypoint.sh
+++ build/files/entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 # setup ros environment, this is copied from the entrypoint script of the normal ROS Dockerfile
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
 # also, source the robosense sdk
-source "/robosense/devel/setup.bash"
+source "/robosense/install/setup.bash"
 
 exec "$@"
