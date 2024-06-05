dc2ddcde846e6a9646de7d137c2fff7e4279c406
Max Viehl <max.viehl@posteo.de>
2024-06-05 12:50:14 +0200

    Adapt to new online_calibration project structure, make rebuild.sh device independent

--- rebuild.sh
+++ rebuild.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
+#
+#  Builds the docker container with RSLidar SDK and online calibration ROS packages.
+#
+#  USAGE:
+#  - Set the environment variable REPO_DIR to the path of the online calibration git repository.
+#  - If you want to mount a data directory (e.g. to play rosbag files in the docker container), then
+#    set the environment variable DATA_DIR to the local data directory.
+#
 
-# tidy up, this might fail
-rm -r /home/bela/git-tmp/robosense_docker/build/files/ros_pkgs/lidar_align
-sudo docker container rm lidar1
-
-# build, exit on error
 set -e
-cp -r /home/bela/git-tmp/trabadyfica/lidar_align /home/bela/git-tmp/robosense_docker/build/files/ros_pkgs/lidar_align
+if [ -z "$REPO_DIR" ]
+then
+    echo "Please set the REPO_DIR variable to the top level directory of the online calibration git repo!"
+    exit 1
+fi
+ROS_PACKAGE_PATH_IN_REPO="online_calibration"  # Adapt to current online calibration repo structure
+
+
+# remove old and copy current online calibration package source before build
+SRC_PATH="./build/files/ros_pkgs/$ROS_PACKAGE_PATH_IN_REPO"
+if [ -d "$SRC_PATH" ]
+then
+    rm -r $SRC_PATH
+fi
+cp -r $REPO_DIR/$ROS_PACKAGE_PATH_IN_REPO $SRC_PATH
+
+# DOCKER BUILD
 sudo docker build --tag lidar ./build  # normal build with network
 # sudo docker build --pull=false --network=none  --tag lidar ./build  # build without network to enforce build caching
 
-# run
-# sudo docker run --name lidar1 -p 5599:5599/udp -p 4499:4499/udp --volume ./config:/ROS_WORKSPACE/rslidar_pkgs/rslidar_sdk/config/:ro -it lidar bash
-sudo   docker run --name lidar1 --net host                        --volume ./config:/ROS_WORKSPACE/rslidar_pkgs/rslidar_sdk/config/:ro -it lidar bash
-
+# DOCKER RUN
+if [ -n "$DATA_DIR" ]
+then
+    DATA_VOLUME_SWITCH="--volume $DATA_DIR:/DATA:ro"
+fi 
+# sudo docker run --rm --name lidar1 -p 5599:5599/udp -p 4499:4499/udp $DATA_VOLUME_SWITCH --volume ./config:/ROS_WORKSPACE/rslidar_pkgs/rslidar_sdk/config/:ro -it lidar bash
+sudo   docker run --rm --name lidar1 --net host                        $DATA_VOLUME_SWITCH --volume ./config:/ROS_WORKSPACE/rslidar_pkgs/rslidar_sdk/config/:ro -it lidar bash
